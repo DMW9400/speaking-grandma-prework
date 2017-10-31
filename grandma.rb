316c42ff9fd50e8251ea47092efe5418a14d95ec
David Welch <dmw9400@gmail.com>
2017-10-31 22:13:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
 
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
